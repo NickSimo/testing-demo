ec0d61cb51dbd935860304e30cb9747ab2a2c29f
NSim <nicolosimoncelli@gmail.com>
2022-05-08 16:42:15 +0200

    First commit

--- src/main/java/com/example/demo/controller/ClientiController.java
+++ src/main/java/com/example/demo/controller/ClientiController.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.Cliente;
 import com.example.demo.service.EstraiClientiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,11 +25,6 @@ public class ClientiController {
     @GetMapping(value = "/estrazione-per-cf")
     public Cliente clienti(@RequestParam String cf) {
         return estraiClientiService.estrazioneClientePerCodiceFiscale(cf);
-    }
-
-    @GetMapping(value = "test/{numero}")
-    public int clienti(@PathVariable int numero) {
-        return numero;
     }
 
 
